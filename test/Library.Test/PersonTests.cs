cadb473f1d0226cbfccdb72cec371454b22b820b
PiliMachado <88661591+PiliMachado@users.noreply.github.com>
2022-04-19 20:28:33 -0300

    Tests

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -1,21 +1,38 @@
 using NUnit.Framework;
-
 using UnitTestAndDebug;
 
 namespace Tests
 {
     public class Tests
     {
+        public string idValid;
+        public Person personaVal;
+
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            string idValid = "5.334.282-7";
+            personaVal = new Person("Pilar Machado", idValid);
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void NotValidNameTest() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            Person personaTes = new Person( null , "5.334.282-7");
+            Assert.AreEqual(personaTes.Name, null);
+        }
+        [Test]
+        public void NotValidIdTest() // Cambiá el nombre para indicar qué estás probando
+        {
+            Person personaTes = new Person( "Pilar Machado" , "5.334.282-7");
+            Assert.AreNotEqual(idValid, personaTes.ID);
+        }
+        [Test]
+        public void ValidIdTest() // Cambiá el nombre para indicar qué estás probando
+        {
+            string idTes = "5.334.282-7";
+            Person personaTes = new Person ("Pilar Machado", idTes);
+            Assert.AreEqual(personaTes.ID, idTes);
         }
     }
 }
